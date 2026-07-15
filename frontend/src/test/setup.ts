0569b174e28968116a914625990947b6c56ecc62
ogithup <s220205007@ankarabilim.edu.tr>
2026-07-15 19:41:21 +0300

    Mock ResizeObserver in frontend tests

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom/vitest";
+
+class MockResizeObserver {
+  observe() {}
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
+Object.defineProperty(globalThis, "ResizeObserver", {
+  writable: true,
+  configurable: true,
+  value: MockResizeObserver,
+});
